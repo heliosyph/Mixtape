5f24e42e354ee6aef451708991c54ed7391bac90
Luke Brinkley <lwbrinkley8@gmail.com>
2022-11-13 17:05:10 -0600

    Updated urls.py  and views.py for Song model.

--- mixtape/urls.py
+++ mixtape/urls.py
@@ -5,4 +5,7 @@ import mixtape
 from mixtape import views
 
 app_name = "mixtape"
-urlpatterns = []
+urlpatterns = [
+    path("Song", views.SongListView.as_view(), name="song_list"),
+    path("Song/<int:pk>/", views.SongDetailView.as_view(), name="song_detail"),
+]
--- mixtape/views.py
+++ mixtape/views.py
@@ -1,4 +1,16 @@
 """views.py."""
 from django.shortcuts import render
+from django.views import generic
+from mixtape.models import Song
 
 # Create your views here.
+
+
+class SongListView(generic.ListView):
+    model = Song
+    template_name = "mixtape/song_list.html"  # file name is lower case
+
+
+class SongDetailView(generic.DetailView):
+    model = Song
+    template_name = "mixtape/song_detail.html"  # file name is lower case
