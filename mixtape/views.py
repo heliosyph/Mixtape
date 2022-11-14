ae18f33c208a776160b4d2152d9884af9d2880b7
heliosyph <byungiselle@gmail.com>
2022-11-13 21:27:40 -0600

    liked_list.html, liked_detail.html and added pages to urls.py

--- mixtape/views.py
+++ mixtape/views.py
@@ -1,7 +1,7 @@
 """views.py."""
 from django.shortcuts import render
 from django.views import generic
-from mixtape.models import Song
+from mixtape.models import Song, Liked
 
 # Create your views here.
 
@@ -18,3 +18,17 @@ class SongDetailView(generic.DetailView):
 
     model = Song
     template_name = "mixtape/song_detail.html"  # file name is lower case
+
+
+class LikedListView(generic.ListView):
+    """Liked List class."""
+
+    model = Liked
+    template_name = "mixtape/liked_list.html"
+
+
+class LikedDetailView(generic.DetailView):
+    """Liked Detail class."""
+
+    model = Liked
+    template_name = "mixtape/Liked_detail.html"
