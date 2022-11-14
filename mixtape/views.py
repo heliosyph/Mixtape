8f9fae93d3175d7b8b4e61da031187057f9a7f39
Cameron Washington <89416312+C00439696@users.noreply.github.com>
2022-11-14 16:02:05 -0600

    Playlist Model

--- mixtape/views.py
+++ mixtape/views.py
@@ -1,9 +1,49 @@
 """views.py."""
 from django.shortcuts import render
 from django.views import generic
-from mixtape.models import Song, Liked
+from mixtape.models import Song, Liked, Playlist
 
-# Create your views here.
+
+class PlaylistListView(generic.ListView):
+    """Playlist List class."""
+
+    model = Playlist
+    template_name = "mixtape/playlist_list.html"  # file name is lower case
+
+
+class PlaylistDetailView(generic.DetailView):
+    """Playlist Detail class."""
+
+    model = Playlist
+    template_name = "mixtape/playlist_detail.html"  # file name is lower case
+
+
+class SongListView(generic.ListView):
+    """Song List class."""
+
+    model = Song
+    template_name = "mixtape/song_list.html"  # file name is lower case
+
+
+class SongDetailView(generic.DetailView):
+    """Song Detail class."""
+
+    model = Song
+    template_name = "mixtape/song_detail.html"  # file name is lower case
+
+
+class LikedListView(generic.ListView):
+    """Liked List class."""
+
+    model = Liked
+    template_name = "mixtape/liked_list.html"
+
+
+class LikedDetailView(generic.DetailView):
+    """Liked Detail class."""
+
+    model = Liked
+    template_name = "mixtape/Liked_detail.html"
 
 
 class SongListView(generic.ListView):
