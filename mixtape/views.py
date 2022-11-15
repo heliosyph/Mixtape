34ffd8119528fe0657b469e47c561336fe0e9b4e
Cameron Washington <89416312+C00439696@users.noreply.github.com>
2022-11-14 19:10:03 -0600

    Fix Playlist Model

--- mixtape/views.py
+++ mixtape/views.py
@@ -45,30 +45,3 @@ class LikedDetailView(generic.DetailView):
     model = Liked
     template_name = "mixtape/Liked_detail.html"
 
-
-class SongListView(generic.ListView):
-    """Song List class."""
-
-    model = Song
-    template_name = "mixtape/song_list.html"  # file name is lower case
-
-
-class SongDetailView(generic.DetailView):
-    """Song Detail class."""
-
-    model = Song
-    template_name = "mixtape/song_detail.html"  # file name is lower case
-
-
-class LikedListView(generic.ListView):
-    """Liked List class."""
-
-    model = Liked
-    template_name = "mixtape/liked_list.html"
-
-
-class LikedDetailView(generic.DetailView):
-    """Liked Detail class."""
-
-    model = Liked
-    template_name = "mixtape/Liked_detail.html"
